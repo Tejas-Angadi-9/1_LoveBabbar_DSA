c957d019defa00c86d8854e5fcd6c4c78832fc07
Tejas Angadi <tejasangadi456@gmail.com>
2024-04-01 19:51:12 +0530

    Added the class 3 of DnC -> rat in maze problem

--- 6. Divide & Conquer/CW/Class 3/1_Rat_in_maze.cpp
+++ 6. Divide & Conquer/CW/Class 3/1_Rat_in_maze.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isSafe(int i, int j, int row, int col, int arr[][3], vector<vector<bool>> &visited){
+    if(((i >= 0 && i<row) && (j >= 0 && j < col)) &&
+        // 2nd condition checking if the maze in unblocked -> 1
+        (arr[i][j] == 1) &&
+        // 3rd condition checking if the way is visited already ie. -> 0/false
+        (visited[i][j] == false)
+        
+    ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 void solveMaze(int arr[3][3], int row, int col, int i, int j, vector<vector<bool>> &visited, vector<string> &path, string output){
     // base case
     if(i == row-1 && j == col-1){
@@ -10,9 +25,41 @@ void solveMaze(int arr[3][3], int row, int col, int i, int j, vector<vector<bool
 
     //* We will solve for 1 case. Rest will be handled by the recursion
     // DOWN ->  (i+1, j)
+    if(isSafe(i+1, j, row, col, arr, visited)){
+        // Mark the visited matrix as true
+        visited[i+1][j] = true;
+        // rc
+        solveMaze(arr, row, col, i+1, j, visited, path, output+'D');
+        // Backtrack
+        visited[i+1][j] = false;
+    }
     // LEFT ->  (i, j-1)
+    if(isSafe(i, j-1, row, col, arr, visited)){
+        // Mark the visited matrix as true
+        visited[i][j-1] = true;
+        // rc
+        solveMaze(arr, row, col, i, j-1, visited, path, output+'L');
+        // Backtrack
+        visited[i][j-1] = false;
+    }
     // RIGHT -> (i, j+1)
+    if(isSafe(i, j+1, row, col, arr, visited)){
+        // Mark the visited matrix as true
+        visited[i][j+1] = true;
+        // rc
+        solveMaze(arr, row, col, i, j+1, visited, path, output+'R');
+        // Backtrack
+        visited[i][j+1] = false;
+    }
     // UP ->    (i-1, j)
+    if(isSafe(i-1, j, row, col, arr, visited)){
+        // Mark the visited matrix as true
+        visited[i-1][j] = true;
+        // rc
+        solveMaze(arr, row, col, i-1, j, visited, path, output+'U');
+        // Backtrack
+        visited[i-1][j] = false;
+    }
 }
 
 int main(){
@@ -22,7 +69,19 @@ int main(){
         {1,1,1},
     };
 
+    // If src is blocked
+    if(maze[0][0] == 0){
+        cout<<"No paths found"<<endl;
+        return 0;
+    }
+
     int row = 3, col = 3;
+    // If dest is blocked
+    if(maze[row-1][col-1] == 0){
+        cout<<"No paths found"<<endl;
+        return 0;
+    }
+
 
     // Create a visited matrix with all the values set to false
     vector<vector<bool>>visited (row, vector<bool>(col, false));
@@ -37,4 +96,9 @@ int main(){
     //* Source of the rat's starting position from 0,0 indices
     int i = 0, j = 0;
     solveMaze(maze, row, col, i, j, visited, path, output);
+
+    //* Print the paths
+    for(auto it: path){
+        cout<<it<<" ";
+    }
 }
